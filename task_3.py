455a2b6314cd6d730ea5cd02162b5642d66d3a40
Amit Amanna <amit.natheniel@gmail.com>
2021-08-27 15:17:19 +0200

    task_3.py is a clone of task_2

---
+++ task_3.py
@@ -0,0 +1,71 @@
+import copy
+
+board = [[15,2,1,12],
+         [8,5,6,11],
+         [4,9,10,7],
+         [3,14,13,0]]
+
+def display_board():
+    for row in board:
+        print(row)
+
+
+def find_gap():
+    for row_index in range(len(board)):
+        for col_index in range(len(board[0])):
+            if board[row_index][col_index] == 0:
+                return row_index, col_index
+
+
+def check_move(row, col, gap_row, gap_col):
+    if row < 0 or row > len(board) or col < 0 or col > len(board[0]):
+        return False
+
+    row_adjacency = (row == gap_row - 1) or (row == gap_row + 1)
+    col_adjacency = (col == gap_col - 1) or (col == gap_col + 1)
+
+    return row_adjacency ^ col_adjacency
+
+
+def do_move(row, col, gap_row, gap_col):
+    global board
+    temp_board = copy.deepcopy(board)
+
+    temp_board[row][col] = board[gap_row][gap_col]
+    temp_board[gap_row][gap_col] = board[row][col]
+
+    board = temp_board
+
+
+def check_win():
+    current_num = 0
+
+    for i in board:
+        for value in i:
+            if value == 0:
+                continue
+            if value == current_num + 1:
+                current_num = value
+            else:
+                return True
+    return False
+
+
+if __name__ == '__main__':
+
+    while check_win():
+        display_board()
+
+        gap_row, gap_col = find_gap()
+
+        pos = input("Type the position of the tile you want to move.")
+        pos_row, pos_col = pos.split(",")
+        row = int(pos_row)
+        col = int(pos_col)
+
+        if check_move(row, col, gap_row, gap_col):
+            do_move(row, col, gap_row, gap_col)
+        else:
+            print("This move won't work... please choose another tile to move")
+
+    print("You Won!!! Congrats")
