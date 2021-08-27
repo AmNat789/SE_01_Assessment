b9aa688fed15cc61acfad644ad2adac1837ea24b
Amit Amanna <amit.natheniel@gmail.com>
2021-08-27 14:44:37 +0200

    Task 1 complete

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import copy
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+board = [[1, 8, 2],
+         [4, 3, 5],
+         [7, 6, 0]]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+winning_board = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 0]]
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def display_board():
+    for row in board:
+        print(row)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def find_gap():
+    for row_index in range(len(board)):
+        for col_index in range(len(board[0])):
+            if board[row_index][col_index] == 0:
+                return row_index, col_index
+
+
+def check_move(row, col, gap_row, gap_col):
+    if row < 0 or row > len(board) or col < 0 or col > len(board[0]):
+        return False
+
+    row_adjacency = (row == gap_row - 1) or (row == gap_row + 1)
+    col_adjacency = (col == gap_col - 1) or (col == gap_col + 1)
+
+    return row_adjacency ^ col_adjacency
+
+
+def do_move(row, col, gap_row, gap_col):
+    global board
+    temp_board = copy.deepcopy(board)
+
+    temp_board[row][col] = board[gap_row][gap_col]
+    temp_board[gap_row][gap_col] = board[row][col]
+
+    board = temp_board
+
+
+if __name__ == '__main__':
+
+    while board != winning_board:
+        display_board()
+
+        gap_row, gap_col = find_gap()
+
+        pos = input("Type the position of the tile you want to move.")
+        pos_row, pos_col = pos.split(",")
+        row = int(pos_row)
+        col = int(pos_col)
+
+        if check_move(row, col, gap_row, gap_col):
+            do_move(row, col, gap_row, gap_col)
+        else:
+            print("This move won't work... please choose another tile to move")
+
+    print("You Won!!! Congrats")
